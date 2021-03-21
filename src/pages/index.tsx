94dcda0fa375474e7039c903b9b3b67015e13442
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-03-21 03:23:31 +0000

    initial index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import { IContentStructure } from '../content';
+import heroImage from '../images/hero-image.svg';
 
 config.autoAddCss = false;
 /* import Banner from '../components/sections/banner';
@@ -16,6 +17,33 @@ import Portfolio from '../components/sections/portfolio'; */
 export default ({ data }: IContentStructure) => (
   <Layout isNotFound={false}>
     <main className="max-w-4xl flex-grow mx-auto flex flex-col justify-around">
+      <div className="sm:flex sm:flex-row-reverse sm:items-center">
+        <div className="sm:px-2">
+          <h1 className="px-4 pt-5 text-2xl text-left text-primary font-bold sm:text-3xl">Base website</h1>
+          <p className="px-4 mt-8 text-lg text-primary sm:mt-8">This is just a place holder, soon it will start having content</p>
+        </div>
+        <img className="w-full max-w-lg mt-16 mx-auto sm:w-1/2" src={heroImage} alt="Gatsby and Tailwind CSS together" />
+      </div>
+
+      <button className="btn btn-green m-auto my-5" type="button">
+        <a href="https://github.com/afonsojramos/afonsojramos.e" className="p-4 text-white text-xs">
+          View on GitHub
+        </a>
+      </button>
+
+      <div className="flex items-center p-3 mx-2 bg-white rounded shadow-xs sm:mx-auto">
+        <code className="text-secondary text-xs leading-xl">npx afonsojramos</code>
+      </div>
+      <div className="mt-12 lg:mt-32 lg:m-auto text-center">
+        <button
+          type="button"
+          className="flex items-center justify-center w-12 h-12 rounded-full bg-cool-gray-100 text-gray-800 animate-bounce hover:text-gray-900 hover:bg-cool-gray-50 transition duration-300 ease-in-out cursor-pointer"
+        >
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+          </svg>
+        </button>
+      </div>
       {/* <Banner data={data.hero.edges} />
       <About data={data.about.edges} />
       <Timeline data={data.experience.edges} />
