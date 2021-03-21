d07187d212eb1465219bca7608d9991779da7594
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-03-21 03:19:51 +0000

    tailwind config tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,39 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      gray: colors.coolGray,
-      blue: colors.lightBlue[100],
-      red: colors.rose,
-      pink: colors.fuchsia,
-    },
     fontFamily: {
       sans: ['Recursive', 'sans-serif'],
     },
+    extend: {
+      colors: {
+        background: colors.gray[300],
+        theme: colors.lightBlue[100],
+        gray: colors.coolGray[200],
+        blue: colors.lightBlue[400],
+        white: colors.white,
+        darkGrey: colors.gray[800],
+      },
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      backgroundColor: {
+        primary: 'var(--color-bg-primary)',
+        secondary: 'var(--color-bg-secondary)',
+      },
+      textColor: {
+        accent: 'var(--color-text-accent)',
+        primary: 'var(--color-text-primary)',
+        secondary: 'var(--color-text-secondary)',
+      },
+    },
   },
+  darkMode: 'media',
   variants: {},
   plugins: [],
 };
