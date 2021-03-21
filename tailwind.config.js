08c8f39f62b1ec5a472a77fe263f056f99d105fe
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-03-21 03:29:55 +0000

    fixed missing css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
       sans: ['Recursive', 'sans-serif'],
