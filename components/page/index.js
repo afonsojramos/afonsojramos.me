fca110542c3ded0ccb7c8ac47ff975d680760f70
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-04-25 15:09:54 +0100

    page module

---
+++ components/page/index.js
@@ -0,0 +1,28 @@
+import Head from '@components/head'
+import Header from '@components/header'
+import styles from './page.module.css'
+
+const Page = ({
+  header = true,
+  footer = true,
+  title,
+  description,
+  image,
+  showHeaderTitle = true,
+  children
+}) => {
+  return (
+    <div className={styles.wrapper}>
+      <Head
+        title={`${title ? `${title} - ` : ''}Afonso Ramos`}
+        description={description}
+        image={image}
+      />
+
+      {header && <Header title={showHeaderTitle && title} />}
+      <main className={styles.main}>{children}</main>
+    </div>
+  )
+}
+
+export default Page
