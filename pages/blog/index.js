df8acf8219196f5aa092d59cb261de9d82eedc27
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-04-25 17:40:31 +0100

    added blog post support

---
+++ pages/blog/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Page from '@components/page'
+import PostsList from '@components/posts-list'
+import getPosts from '@lib/get-posts'
+
+const Blog = ({ posts }) => {
+  return (
+    <Page title="Blog" description="Writing about whatever comes to mind.">
+      <article>
+        <ul>
+          <PostsList posts={posts} />
+        </ul>
+      </article>
+    </Page>
+  )
+}
+
+export const getStaticProps = () => {
+  const posts = getPosts()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Blog
