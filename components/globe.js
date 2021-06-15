30095222dd3225fffd3524d014d82a46f09e0f77
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-06-15 10:46:26 +0100

    globe mobile support & extra features

--- components/globe.js
+++ components/globe.js
@@ -5,12 +5,30 @@ if (typeof window !== 'undefined') Globe = require('react-globe.gl').default
 
 import countryLists from '@data/countries.json'
 
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  })
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+  }, [])
+  return windowSize
+}
+
 const GlobeWrapper = () => {
   const [imageUrl] = useState('/CFCFCF.png')
   const globeRef = useRef(null)
   const [countries, setCountries] = useState({ features: [] })
   const { lived, visited } = countryLists
   const [mounted, setMounted] = useState(false)
+  const size = useWindowSize()
 
   useEffect(() => {
     // load data
@@ -24,8 +42,8 @@ const GlobeWrapper = () => {
     mounted && (
       <Globe
         ref={globeRef}
-        width={700}
-        height={700}
+        width={(size.width > 720 && 720) || size.width - 32}
+        height={(size.width > 720 && 720) || size.height / 2}
         backgroundColor={'rgba(0,0,0,0)'}
         globeImageUrl={imageUrl}
         polygonsData={countries.features.filter(
@@ -45,7 +63,16 @@ const GlobeWrapper = () => {
           <b>${properties.ADMIN} (${properties.ISO_A2})</b>
           `}
         rendererConfig={{ preserveDrawingBuffer: true }}
-        /* controls={remove zoom add autorotation} */
+        pointOfView={{ lng: 30, lat: 30 }}
+        controls={
+          globeRef &&
+          globeRef.current &&
+          (globeRef.current.controls().autoRotate = true) &&
+          (globeRef.current.controls().autoRotateSpeed = 0.5) &&
+          (globeRef.current.controls().enableZoom = false) &&
+          (globeRef.current.controls().enableDamping = true) &&
+          (globeRef.current.controls().dampingFactor = 100)
+        }
       />
     )
   )
