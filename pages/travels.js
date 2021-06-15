f77708ab0e66f4f890b1831fd7f342e68ea085bf
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-06-15 11:48:46 +0100

    added markdown support to travels page

--- pages/travels.js
+++ pages/travels.js
@@ -1,15 +1,27 @@
 import Page from '@components/page'
 import Globe from '@components/globe'
+import getMarkdown from '@lib/get-markdown'
 
-const Travels = () => {
+const Travels = ({ html }) => {
   return (
     <Page
       title="World Travels"
       description="Register of places I've visited so far."
     >
       <Globe />
+      <article dangerouslySetInnerHTML={{ __html: html }} />
     </Page>
   )
+}
+
+export const getStaticProps = async () => {
+  const md = await getMarkdown('data/travels.md')
+
+  return {
+    props: {
+      html: md,
+    },
+  }
 }
 
 export default Travels
