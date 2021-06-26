ad8df55fa5f338cf45508a26edcdbd122d561198
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-06-26 17:50:24 +0100

    Add 404 lottie animation

--- components/error/index.js
+++ components/error/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
+import Lottie from 'react-lottie-player'
+import { replaceColor } from 'lottie-colorify'
+import { useTheme } from 'next-themes'
 
 import Page from '@components/page'
 import Link from '@components/link'
+import lottie404 from '@components/icons/lottie/404.json'
 import styles from './error.module.css'
 
 const Error = ({ status }) => {
+  const { theme } = useTheme()
   return (
     <Page title={status || 'Error'}>
       <Head>
@@ -13,20 +18,34 @@ const Error = ({ status }) => {
 
       {status === 404 ? (
         <>
+          <Lottie
+            loop
+            animationData={
+              theme === 'dark'
+                ? replaceColor('#000000', '#ffffff', lottie404)
+                : lottie404
+            }
+            play
+            style={{
+              width: 300,
+              height: 300,
+              margin: '0 auto',
+              marginBottom: '-50px',
+              marginTop: '-50px',
+            }}
+          />
           <h1>This page cannot be found.</h1>
 
-          <div>
-            <blockquote cite="https://afonsojramos.me/">
-              <p>The perfect personal website doesn't exis-</p>
+          <blockquote cite="https://afonsojramos.me/">
+            <p>The perfect personal website doesn't exis-</p>
 
-              <footer>
-                — You, before visiting this{' '}
-                <Link external href="https://afonsojramos.me/">
-                  <cite>website</cite>
-                </Link>
-              </footer>
-            </blockquote>
-          </div>
+            <footer>
+              — You, before visiting this{' '}
+              <Link external href="https://afonsojramos.me/">
+                <cite>website</cite>
+              </Link>
+            </footer>
+          </blockquote>
         </>
       ) : (
         <section className={styles.section}>
