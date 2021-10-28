e7d547991d3f7a9a32c8c64f0eb602e600111fc2
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-10-28 11:47:53 +0100

    Add support for homepage views

--- pages/api/views/[slug].js
+++ pages/api/views/[slug].js
@@ -1,8 +1,13 @@
 import db from '@lib/firebase'
 
 export default async (req, res) => {
+  const ref =
+    req.query.slug === 'home'
+      ? db.ref('home')
+      : db.ref('views').child(req.query.slug)
+
   if (req.method === 'GET') {
-    const snapshot = await db.ref('views').child(req.query.slug).once('value')
+    const snapshot = await ref.once('value')
     const views = snapshot.val()
 
     return res.status(200).json({ total: views })
@@ -10,7 +15,6 @@ export default async (req, res) => {
 
   if (req.method === 'POST') {
     await new Promise((resolve) => setTimeout(resolve, 500))
-    const ref = db.ref('views').child(req.query.slug)
     const { snapshot } = await ref.transaction((currentViews) => {
       if (currentViews === null) {
         return '–––'
