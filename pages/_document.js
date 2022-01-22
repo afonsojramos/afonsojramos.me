6a77d80873f2ec11b67c4ddbf4fcd4548ec4e0d8
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-01-22 03:16:30 +0000

    add: fair analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            defer
+            data-domain-id="ad5b2dc6392a0ea4077ba9922d11d191cdccbf78"
+            src="https://fairdatacenter.de/cdn/fair.js"
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
