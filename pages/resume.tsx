e8a5533dd9767e8efc0a62888da0d3fc6b88b5af
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-21 10:10:47 +0200

    fix command component

---
+++ pages/resume.tsx
@@ -0,0 +1,13 @@
+import Page from '@components/page'
+
+const Resume = () => {
+  return (
+    <Page title="Resume" description="Check what I've done!">
+      <article>
+        <p>Get in touch!</p>
+      </article>
+    </Page>
+  )
+}
+
+export default Resume
