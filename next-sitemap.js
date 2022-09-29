d05d2d3d238d31133ddd176749e915a94e19e74c
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-09-29 15:12:04 +0100

    chore: generate single sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteUrl: 'https://afonsojramos.me',
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
   transform: async (config, path) => {
     return {
       loc: path,
