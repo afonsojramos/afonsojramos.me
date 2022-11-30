232bd42968ae789766433a62c93888adec735372
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-11-30 15:38:44 +0000

    chore: move firebase variables to env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,10 +5,9 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      projectId: 'afonsojramos-me'
+      projectId: process.env.FIREBASE_PROJECT_ID
     }),
-    databaseURL:
-      'https://afonsojramos-me-default-rtdb.europe-west1.firebasedatabase.app/'
+    databaseURL: process.env.FIREBASE_DATABASE_URL
   })
 }
 
