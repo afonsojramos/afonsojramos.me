7f8e862a4c59527d251e382ecc1355ce81d2cc39
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-12-03 16:25:49 +0000

    feat(Music): improve mobile usage

--- components/device/index.tsx
+++
@@ -1,5 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const Device = dynamic(() => import('./device'), { ssr: false })
-
-export default Device
--- pages/music.tsx
+++ pages/music.tsx
@@ -1,27 +1,22 @@
-import { useState } from 'react'
-import useDelayedRender from 'use-delayed-render'
-import cn from 'classnames'
-import Page from '@components/page'
-import Entry from '@components/entry'
-import Link from '@components/link'
-import Device from '@components/device'
+import { useState } from 'react';
+import cn from 'classnames';
+import Page from '@components/page';
+import Entry from '@components/entry';
+import Link from '@components/link';
 
-import entryStyles from '../components/entry/entry.module.css'
-import commandStyles from '../components/command/command.module.css'
-import styles from '../components/page/page.module.css'
-import { DialogContent, DialogOverlay } from '@reach/dialog'
+import entryStyles from '../components/entry/entry.module.css';
+import commandStyles from '../components/command/command.module.scss';
+import styles from '../components/page/page.module.css';
 
 // Data
-import music from '@data/music.json'
-import NowPlaying from '@components/now-playing'
+import music from '@data/music.json';
+import NowPlaying from '@components/now-playing';
+import * as Popover from '@radix-ui/react-popover';
+import { isMobile, MobileView } from 'react-device-detect';
 
 const Music = () => {
-  const { data: years } = music
-  const [modalShow, setModalShow] = useState(false)
-  const { mounted, rendered } = useDelayedRender(modalShow, {
-    enterDelay: -1,
-    exitDelay: 200
-  })
+  const { data: years } = music;
+  const [modalShow, setModalShow] = useState(false);
 
   const YearList = () => (
     <div className={cn(styles.sidenav)}>
@@ -33,81 +28,79 @@ const Music = () => {
                 {year}
               </Link>
             </li>
-          )
+          );
         })}
       </ul>
     </div>
-  )
+  );
 
   return (
     <Page
       title="Music"
       description="Collection of pictures of the most memorable concerts so far."
     >
-      <h1>Favorite Albums & Concerts</h1>
-      <NowPlaying bigPicture />
-      <Device>
-        {({ isMobile }: { isMobile: boolean }) => {
-          if (isMobile)
-            return (
-              <DialogOverlay
-                isOpen={mounted}
-                className={cn(commandStyles.screen, {
-                  [commandStyles.show]: rendered
-                })}
-                onDismiss={() => setModalShow(false)}
+      <Popover.Root modal open={modalShow} onOpenChange={setModalShow}>
+        <h1>Favorite Albums & Concerts</h1>
+        <NowPlaying bigPicture />
+        <Popover.Content>
+          <MobileView>
+            <div
+              className={cn(commandStyles.screen, {
+                [commandStyles.show]: modalShow
+              })}
+            >
+              <div
+                style={{
+                  boxShadow: '0px 10px 50px hsla(0, 0%, 0%, 0.33)'
+                }}
+                className={commandStyles['dialog-content']}
+                aria-label="Year Selector"
               >
-                <DialogContent
-                  style={{ boxShadow: '0px 10px 50px hsla(0, 0%, 0%, 0.33)' }}
-                  className={commandStyles['dialog-content']}
-                  aria-label="Year Selector"
-                >
-                  <YearList />
-                </DialogContent>
-              </DialogOverlay>
-            )
-          return <YearList />
-        }}
-      </Device>
-      <article>
-        {years.map(({ year, description, concerts, albums }) => {
-          return (
-            <div key={year} className={styles.musicYear}>
-              <h2 id={year} onClick={() => setModalShow(true)}>
-                {year}
-              </h2>
-              <h3>{description}</h3>
-              <div>
-                {concerts?.map((entry) => {
-                  return (
-                    <Entry
-                      key={entry.title}
-                      title={entry.title}
-                      image={entry.image}
-                      description={entry.festival}
-                    />
-                  )
-                })}
-              </div>
-              <div className={entryStyles.grid}>
-                {albums.map((entry) => {
-                  return (
-                    <Entry
-                      key={`${entry.title} - ${entry.artist}`}
-                      title={entry.title}
-                      image={entry.image}
-                      href={entry.url}
-                      description={entry.artist}
-                    />
-                  )
-                })}
+                <YearList />
               </div>
             </div>
-          )
-        })}
-      </article>
+          </MobileView>
+        </Popover.Content>
+        <article>
+          {years.map(({ year, description, concerts, albums }) => {
+            return (
+              <div key={year} className={styles.musicYear}>
+                <h2 id={year} onClick={() => isMobile && setModalShow(true)}>
+                  {year}
+                </h2>
+                <h3>{description}</h3>
+                <div>
+                  {concerts?.map((entry) => {
+                    return (
+                      <Entry
+                        key={entry.title}
+                        title={entry.title}
+                        image={entry.image}
+                        description={entry.festival}
+                      />
+                    );
+                  })}
+                </div>
+                <div className={entryStyles.grid}>
+                  {albums.map((entry) => {
+                    return (
+                      <Entry
+                        key={`${entry.title} - ${entry.artist}`}
+                        title={entry.title}
+                        image={entry.image}
+                        href={entry.url}
+                        description={entry.artist}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </article>
+      </Popover.Root>
     </Page>
-  )
-}
+  );
+};
 
-export default Music
+export default Music;
