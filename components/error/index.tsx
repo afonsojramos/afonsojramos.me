e4889ea5f8be37dc249e73973621dbf5adb9e0c8
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-12-05 03:14:06 +0000

    feat(error): overhaul to support loading & error

--- components/error/index.tsx
+++ components/error/index.tsx
@@ -6,27 +6,33 @@ import { useTheme } from 'next-themes';
 import Page from '@components/page';
 import Link from '@components/link';
 import lottie404 from '@components/icons/lottie/404.json';
-import styles from './error.module.css';
+import lottieloading from '@components/icons/lottie/loading.json';
+import styles from './error.module.scss';
 
-const Error = ({ status }: { status: number }) => {
+const Error = ({
+  title,
+  loading = false
+}: {
+  title?: string;
+  loading?: boolean;
+}) => {
   const { theme } = useTheme();
-  const animationData: JSON =
+  const animation404: JSON =
     theme === 'dark'
       ? replaceColor('#000000', '#ffffff', lottie404)
       : lottie404;
 
   return (
-    <Page title={`${status}` || 'Error'} description={'Error 404'}>
-      <Head>
-        <title>404 — Afonso</title>
-      </Head>
-
-      {status === 404 ? (
+    <Page title={`${title}` || 'Error'}>
+      {title === '404' ? (
         <>
+          <Head>
+            <title>404 — Afonso</title>
+          </Head>
           <Lottie
             play
             loop
-            animationData={animationData}
+            animationData={animation404}
             style={{
               width: 300,
               height: 300,
@@ -51,8 +57,43 @@ const Error = ({ status }: { status: number }) => {
         </>
       ) : (
         <section className={styles.section}>
-          <span>{status || '?'}</span>
-          <p>An error occurred.</p>
+          {loading ? (
+            <Lottie
+              play
+              loop
+              animationData={lottieloading}
+              style={{
+                width: 300,
+                height: 300,
+                margin: '0 auto',
+                marginBottom: '-50px',
+                marginTop: '-50px'
+              }}
+              audioFactory={undefined}
+            />
+          ) : (
+            <>
+              <h3>This definitely should not have happened.</h3>
+
+              <blockquote cite="https://afonsojramos.me/">
+                <p>
+                  Going fast makes you focus on what&apos;s important;
+                  <br />
+                  there&apos;s no time for bullshit
+                </p>
+
+                <footer>— Me, trying to find excuses for this 💀 </footer>
+              </blockquote>
+
+              <p>
+                Please go back to the{' '}
+                <Link href="https://afonsojramos.me/" underline>
+                  homepage
+                </Link>{' '}
+                as that is the thing <b>I&apos;m sure</b> is not broken.
+              </p>
+            </>
+          )}
         </section>
       )}
     </Page>
