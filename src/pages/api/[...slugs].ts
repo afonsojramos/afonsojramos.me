dd3df6b992502f5b7e89466bfc908eccd80754c3
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2025-03-07 02:53:09 +0000

    feat: add Cloudflare adapter and Elysia API route

---
+++ src/pages/api/[...slugs].ts
@@ -0,0 +1,16 @@
+import { Elysia, t } from "elysia";
+
+export const prerender = false;
+
+const app = new Elysia({ prefix: "/api" })
+  .get("/", () => "hi")
+  .post("/", ({ body }) => body, {
+    body: t.Object({
+      name: t.String(),
+    }),
+  });
+
+const handle = ({ request }: { request: Request }) => app.handle(request);
+
+export const GET = handle;
+export const POST = handle;
