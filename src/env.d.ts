06b0ec61c2e9452b0b97e9e1ff4b2ecad9310ede
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2025-03-11 03:41:42 +0000

    feat: add Cloudflare runtime type for Astro locals

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare namespace App {
+  interface Locals extends Runtime {}
+}
