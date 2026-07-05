ee9accdff1ec41643023a99e70476515b11a4cb8
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2026-07-05 23:55:26 +0200

    fix(rss): limit feed to blog posts only

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -10,13 +10,9 @@ type Context = {
 };
 
 export async function GET(context: Context) {
-  const blog = (await getCollection("blog")).filter((post) => !post.data.draft);
-
-  const projects = (await getCollection("projects")).filter((project) => !project.data.draft);
-
-  const items = [...blog, ...projects].sort(
-    (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
-  );
+  const items = (await getCollection("blog"))
+    .filter((post) => !post.data.draft)
+    .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
 
   const renderedItems = await Promise.all(
     items.map(async (item) => ({
